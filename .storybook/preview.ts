c12f91ce3cddae65e0f40345ca3f05f85ce6a76e
Denys Bokov <denisbokov1703@gmail.com>
2024-03-10 20:07:05 +0100

    added INITIAL_VIEWPORTS

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,5 @@
 import type { Preview } from "@storybook/react";
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport'
 
 const preview: Preview = {
   parameters: {
@@ -9,6 +10,9 @@ const preview: Preview = {
         date: /Date$/i,
       },
     },
+    viewport: {
+      viewports: INITIAL_VIEWPORTS,
+    }
   },
 };
 
