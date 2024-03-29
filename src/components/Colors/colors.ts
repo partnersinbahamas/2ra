16ea6c31cc1853733f9231be874e6f292cca6d8d
Denys Bokov <denisbokov1703@gmail.com>
2024-03-29 19:51:44 +0100

    colors stories mdx implemented

---
+++ src/components/Colors/colors.ts
@@ -0,0 +1,120 @@
+// DB COLORS
+export const WHITE = '#ffffff';
+export const BLACK = '#000000';
+// Yellow
+export const YELLOW_800 = '#ff7a00';
+export const YELLOW_700 = '#ff9b00';
+export const YELLOW_600 = '#ffbb00';
+export const YELLOW_500 = '#ffd800';
+export const YELLOW_400 = '#fff000';
+export const YELLOW_300 = '#fff876';
+export const YELLOW_200 = '#ffffaf';
+export const YELLOW_100 = '#ffffdc';
+// Orange
+export const ORANGE_800 = '#a24800';
+export const ORANGE_700 = '#c05e00';
+export const ORANGE_600 = '#d77b00';
+export const ORANGE_500 = '#f39200';
+export const ORANGE_400 = '#f8ab37';
+export const ORANGE_300 = '#faca7f';
+export const ORANGE_200 = '#fce3b4';
+export const ORANGE_100 = '#fff4d8';
+// Red
+export const RED_800 = '#740009';
+export const RED_700 = '#9b000e';
+export const RED_600 = '#c50014';
+export const RED_500 = '#ec0016'; // DB Rot
+export const RED_400 = '#f75056';
+export const RED_300 = '#fa9090';
+export const RED_200 = '#fcc8c3';
+export const RED_100 = '#fee6e6';
+// Burgundy
+export const BURGUNDY_800 = '#4d0820';
+export const BURGUNDY_700 = '#641e32';
+export const BURGUNDY_600 = '#8c2e46';
+export const BURGUNDY_500 = '#a9455d';
+export const BURGUNDY_400 = '#c0687b';
+export const BURGUNDY_300 = '#da9aa8';
+export const BURGUNDY_200 = '#edcbd6';
+export const BURGUNDY_100 = '#f4e8ed';
+// Pink
+export const PINK_800 = '#970052';
+export const PINK_700 = '#b80065';
+export const PINK_600 = '#db0078';
+export const PINK_500 = '#e93e8f';
+export const PINK_400 = '#ee7bae';
+export const PINK_300 = '#f4aece';
+export const PINK_200 = '#f9d2e5';
+export const PINK_100 = '#fdeef8';
+// Violet
+export const VIOLET_800 = '#421857';
+export const VIOLET_700 = '#581d70';
+export const VIOLET_600 = '#6e368c';
+export const VIOLET_500 = '#814997';
+export const VIOLET_400 = '#9a6ca6';
+export const VIOLET_300 = '#c2a1c7';
+export const VIOLET_200 = '#e0cde4';
+export const VIOLET_100 = '#f4eefa';
+// Blue
+export const BLUE_800 = '#061350';
+export const BLUE_700 = '#0a1e6e';
+export const BLUE_600 = '#0c3992';
+export const BLUE_500 = '#1455c0';
+export const BLUE_400 = '#347de0';
+export const BLUE_300 = '#73aef4';
+export const BLUE_200 = '#b4d5f6';
+export const BLUE_100 = '#e0effb';
+// Cyan
+export const CYAN_800 = '#004b6d';
+export const CYAN_700 = '#006a96';
+export const CYAN_600 = '#0087b9';
+export const CYAN_500 = '#309fd1';
+export const CYAN_400 = '#55b9e6';
+export const CYAN_300 = '#84cfef';
+export const CYAN_200 = '#bbe6f8';
+export const CYAN_100 = '#e5faff';
+// Turqouise
+export const TURQUOISE_800 = '#005752';
+export const TURQUOISE_700 = '#006e6b';
+export const TURQUOISE_600 = '#008984';
+export const TURQUOISE_500 = '#00a099';
+export const TURQUOISE_400 = '#3cb5ae';
+export const TURQUOISE_300 = '#83caca';
+export const TURQUOISE_200 = '#bee2e5';
+export const TURQUOISE_100 = '#e3f5f4';
+// Green
+export const GREEN_800 = '#154a26';
+export const GREEN_700 = '#165c27';
+export const GREEN_600 = '#2a7230';
+export const GREEN_500 = '#408335';
+export const GREEN_400 = '#66a558';
+export const GREEN_300 = '#8cbc80';
+export const GREEN_200 = '#bddbb9';
+export const GREEN_100 = '#e2f3e5';
+// Light Green
+export const LIGHT_GREEN_800 = '#375f15';
+export const LIGHT_GREEN_700 = '#44741a';
+export const LIGHT_GREEN_600 = '#508b1b';
+export const LIGHT_GREEN_500 = '#63a615';
+export const LIGHT_GREEN_400 = '#78be14';
+export const LIGHT_GREEN_300 = '#9fd45f';
+export const LIGHT_GREEN_200 = '#c9eb9e';
+export const LIGHT_GREEN_100 = '#ebf7dd';
+// Warm Gray
+export const WARM_GRAY_800 = '#38342f';
+export const WARM_GRAY_700 = '#4f4b41';
+export const WARM_GRAY_600 = '#747067';
+export const WARM_GRAY_500 = '#858379';
+export const WARM_GRAY_400 = '#9c9a8e';
+export const WARM_GRAY_300 = '#bcbbb2';
+export const WARM_GRAY_200 = '#ddded6';
+export const WARM_GRAY_100 = '#f5f4f1';
+// Cool Gray
+export const COOL_GRAY_800 = '#131821';
+export const COOL_GRAY_700 = '#282d37';
+export const COOL_GRAY_600 = '#3c414b';
+export const COOL_GRAY_500 = '#646973';
+export const COOL_GRAY_400 = '#878c96';
+export const COOL_GRAY_300 = '#afb4bb';
+export const COOL_GRAY_200 = '#d7dce1';
+export const COOL_GRAY_100 = '#f0f3f5';
