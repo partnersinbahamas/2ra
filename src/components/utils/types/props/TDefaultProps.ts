3d72c4f42d049d0f18b0ef14c9762a9cbbf04451
Denys Bokov <denisbokov1703@gmail.com>
2024-05-04 15:12:57 +0200

    feat(type): create TDefaultProps type

---
+++ src/components/utils/types/props/TDefaultProps.ts
@@ -0,0 +1,10 @@
+import { TSize, TStatus, TStile } from '../types';
+
+type TDefaultProps = {
+  stile: TStile;
+  size: TSize;
+  status: TStatus;
+  anchorOrigin: IAnchorOrigin;
+};
+
+export default TDefaultProps;
