0e8eb5b07fd2cbb01d80685fc87b11fe402e14ae
Denys Bokov <denisbokov1703@gmail.com>
2024-05-04 15:13:42 +0200

    feat(variables): create defaultProps object veriable with TDefaultProps type

---
+++ src/components/utils/variables/defaultProps.ts
@@ -0,0 +1,13 @@
+import TDefaultProps from '../types/props/TDefaultProps';
+
+const defaultProps: TDefaultProps = {
+  stile: 'default',
+  size: 'medium',
+  status: 'default',
+  anchorOrigin: {
+    vertical: 'bottom',
+    horizontal: 'right',
+  },
+};
+
+export default defaultProps;
