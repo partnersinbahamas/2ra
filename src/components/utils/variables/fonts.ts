ffd23239a207b9628d000becf496b8b2d8c1e919
Denys Bokov <denisbokov1703@gmail.com>
2024-06-04 19:58:47 +0200

    feat(utils): create fonts placeholders and variables

---
+++ src/components/utils/variables/fonts.ts
@@ -0,0 +1,28 @@
+import { css } from 'styled-components';
+export const fontTura = css`
+  font-family: 'Nunito Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif !important;
+`;
+
+export const fontTuraRegular = css`
+  font-family: 'Nunito Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif !important;
+  font-style: normal !important;
+  font-weight: 300 !important;
+`;
+  
+export const fontTuraRegularBold = css`
+  font-family: 'Nunito Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif !important;
+  font-style: normal !important;
+  font-weight: 600 !important;
+`;
+
+export const fontTuraItalic = css`
+  font-family: 'Nunito Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif !important;
+  font-style: italic !important;
+  font-weight: 300 !important;
+`;
+  
+export const fontTuraItalicBold = css`
+  font-family: 'Nunito Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif !important;
+  font-style: italic !important;
+  font-weight: 600 !important;
+`;
