5c3eca2abda297026127e238184ddcb77a8acb41
Denys Bokov <denisbokov1703@gmail.com>
2024-06-28 20:53:29 +0200

    feat(IconButton): create stories with IconButton props

--- src/components/Buttons/IconButton/IconButton.stories.tsx
+++ src/components/Buttons/IconButton/IconButton.stories.tsx
@@ -11,11 +11,16 @@ const meta = {
   parameters: {
     layout: 'centered',
   },
+  args: {
+    labelPosition: 'bottom',
+    children: <DataTreeIcon />,
+  },
   argTypes: {
     children: story.children,
     size: story.size,
     label: story.title,
     onClick: story.onClick,
+    stile: story.stile,
     backgroundColor: story.backgroundColor,
     showLabel: story.boolean,
     labelPosition: story.labelPosition,
@@ -27,6 +32,30 @@ export default meta;
 type TStory = StoryObj<typeof meta>;
 
 export const Default: TStory = { args: { children: <DataTreeIcon /> } };
+export const DefaultLabel: TStory = {
+  args: {
+    stile: 'default',
+    label: 'Default',
+    showLabel: true,
+  },
+};
+
+export const Primary: TStory = {
+  args: {
+    stile: 'primary',
+  },
+};
+
+export const PrimaryLabel: TStory = {
+  args: {
+    stile: 'primary',
+    label: 'Primary',
+    showLabel: true,
+  },
+};
+
+export const Mute: TStory = { args: { stile: 'mute' } };
+
 export const Backgroup: TStory = {
   args: {
     backgroundColor: COOL_GRAY_100,
